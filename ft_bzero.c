ccd60ed04600a809956d9a97e37bd9378fc204e0
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-05-29 16:59:14 +0200

    updated bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 17:01:18 by enayimul          #+#    #+#             */
-/*   Updated: 2019/05/28 17:13:17 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/05/29 16:51:41 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
