f636a85962a93eea088bf3b97b47733217e43d55
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-05-29 16:36:00 +0200

    updated memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,22 +6,34 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 16:44:43 by enayimul          #+#    #+#             */
-/*   Updated: 2019/05/28 16:51:41 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/05/29 12:01:49 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	char *x;
+	char *y;
 
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
+	if (n == 0 || dest == src)
+		return (dest);
+	x = (char *)dest;
+	y = (char *)src;
+	while (--n)
+		*x++ = *y++;
+	*x = *y;
 	return (dest);
 }
+
+int		main()
+{
+	char src[35] = "lebza is coming to school again";
+	char dest[18] = "hello world again";
+
+	printf("%s", ft_memcpy(dest, src, 32));
+	return (0);
+}
+
