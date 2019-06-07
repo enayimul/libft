571988875a9f1ed7d442f45f6071f5fe46c3909e
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-07 15:15:44 +0200

    added memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/07 14:06:17 by enayimul          #+#    #+#             */
+/*   Updated: 2019/06/07 15:13:39 by enayimul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+	char	dst1;
+	char	src1;
+
+	i = 0;
+	dst1 = (char *)dst;
+	src1 = (char *)src;
+	while (i < n)
+	{
+		dst1[i] = src1[i];
+		if ((unsigned char)src1[i] == (unsigned char)c)
+			return ((char *)dst + i + 1);
+		i++;
+	}
+	return (NULL);
+}
