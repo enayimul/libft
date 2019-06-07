68a04fcbe42a7ecbf920b64ad2f2109328f2a5c2
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-07 15:30:54 +0200

    updated memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 14:06:17 by enayimul          #+#    #+#             */
-/*   Updated: 2019/06/07 15:13:39 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/06/07 15:29:38 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t	i;
-	char	dst1;
-	char	src1;
+	char	*dst1;
+	char	*src1;
 
 	i = 0;
 	dst1 = (char *)dst;
