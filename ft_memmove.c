7dbac539bcdf7b4a9e2ff8e1bf56852d3d76dcac
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-07 10:41:11 +0200

    adding stff

---
+++ ft_memmove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 08:57:29 by enayimul          #+#    #+#             */
+/*   Updated: 2019/06/06 12:57:08 by enayimul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*tmp;
+
+	if (dst == '\0' && src == '\0')
+		return (NULL);
+	tmp = (char *)malloc(sizeof(char) * len);
+	if (tmp == NULL)
+		return (NULL);
+	ft_memcpy(tmp, src, len);
+	ft_memcpy(dst, tmp, len);
+	free(tmp);
+	return (dst);
+}
