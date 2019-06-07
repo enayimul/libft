7ffd68c9fba295e6c483826655d77f000350925e
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-07 17:26:39 +0200

    added strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 16:04:25 by enayimul          #+#    #+#             */
-/*   Updated: 2019/06/07 17:10:09 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/06/07 17:25:58 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
 
