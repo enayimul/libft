2a95e1eb37749b5c9bbdb5d3062fa09d64a7daaa
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-08 12:31:37 +0200

    incomplete memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/08 10:59:17 by enayimul          #+#    #+#             */
+/*   Updated: 2019/06/08 12:29:03 by enayimul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = ((char *)s)
+		while (i < n)
+		{
+			if ((unsigned char)str[i] == (unsigned char)s)
+				return ((char *)s + i);
+			i++;
+		}
+	return (NULL);
+}
