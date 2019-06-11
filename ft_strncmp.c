651c755227b01f019efa420956584af5982f7ea6
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-11 14:38:36 +0200

    complete

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 10:15:25 by enayimul          #+#    #+#             */
-/*   Updated: 2019/05/30 09:13:25 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:36:42 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t		i;
 
 	i = 0;
-	if (!n)
-	{
+	if (n == 0)
 		return (0);
-	}
-	while (s1[i] && (s1[i] == s2[i] && --n))
+	while (s1[i] != '\0' && s1[i] == s2[i] && i < n - 1)
 	{
-		s1++;
-		s2++;
+		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
