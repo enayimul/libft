113debc02213fdc89a183562081140491f136d23
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-19 09:46:40 +0200

    complete

---
+++ ft_strmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/18 15:38:51 by enayimul          #+#    #+#             */
+/*   Updated: 2019/06/19 09:45:15 by enayimul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (char)f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
