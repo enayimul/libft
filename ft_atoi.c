b0a655f0910d600811c1cd2de52dd68d95ad34a5
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-21 21:25:59 +0200

    complete

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 09:14:04 by enayimul          #+#    #+#             */
-/*   Updated: 2019/06/06 15:43:48 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/06/21 09:19:14 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 int		ft_atoi(const char *str)
 {
-	long	sign;
-	long	i;
-	long	nbr;
+	int					sign;
+	int					i;
+	unsigned int		nbr;
 
 	i = 0;
 	sign = 1;
 	nbr = 0;
-	while ((str[i] == ' ') || (str[i] == '\t')
-			|| (str[i] == '\v') || (str[i] == '\n')
-			|| (str[i] == '\r') || (str[i] == '\f'))
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
+		if (str[i] == '-')
+			sign = -1;
 		i++;
 	}
-	if (str[i] == '-' || str[i] == '+')
-		if (str[i] == '-')
-		{
-			sign = -1;
-			i++;
-		}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		nbr = 10 * nbr + (str[i] - '0');
 		i++;
 	}
+	if (nbr > 2147483648 && sign == -1)
+		return (0);
+	if (nbr > 2147483647 && sign == 1)
+		return (-1);
 	return (nbr * sign);
 }
