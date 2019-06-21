16f985f89ec96aba4e97cba95061b5f16509e82e
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-21 21:33:52 +0200

    complete

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/20 12:01:44 by enayimul          #+#    #+#             */
+/*   Updated: 2019/06/20 12:11:12 by enayimul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (char)f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
