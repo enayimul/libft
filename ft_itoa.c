58080e70cd1e7b55501f1d2568506f59b1bd20c5
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-06-25 15:46:52 +0200

    complete

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,58 +6,32 @@
 /*   By: enayimul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 09:31:47 by enayimul          #+#    #+#             */
-/*   Updated: 2019/06/24 11:23:15 by enayimul         ###   ########.fr       */
+/*   Updated: 2019/06/25 15:39:27 by enayimul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	level(int n)
+char	*ft_itoa(int n)
 {
-	int		i;
-	int		j;
+	char	*str;
 
-	j = 0;
-	i = 1;
-	if (n < 0)
-		n = -1 * n;
-	if (n == 0)
-		j++;
-	else if (n != 0)
-	{
-		while (n / i != 0)
-		{
-			i = i * 10;
-			j++;
-		}
-	}
-	return (j);
-}
-
-char		*ft_itoa(int n)
-{
-	char	*num;
-	int		i;
-	int		neg;
-
-	num = 0;
-	neg = 0;
-	if (n < 0)
-		neg = 1;
-	i = level(n);
-	if (!(num = (char *)malloc(sizeof(char) * (i + 1 + neg))))
-		return (0);
+	if (!(str = (char *)malloc(sizeof(char) * 2)))
+		return (NULL);
+	if (n == -2147483648)
+		return (ft_strcpy(str, "-2147483648"));
 	if (n < 0)
 	{
-		n = -1 * n;
-		i++;
-		num[0] = '-';
+		str[0] = '-';
+		str[1] = '\0';
+		str = ft_strjoin(str, ft_itoa(-n));
 	}
-	num[i] = '\0';
-	while (--i >= neg)
+	else if (n >= 10)
+		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
+	else if (n < 10 && n >= 0)
 	{
-		num[i] = '0' + (n % 10);
-		n = (n - (n % 10)) / 10;
+		str[0] = n + '0';
+		str[1] = '\0';
 	}
-	return (num);
+	return (str);
 }
